e56f7e7219cb50a48dcbe26b261236c0988370e8
Memnoc <smartdroidesign@gmail.com>
2018-06-15 15:53:01 +0100

    Initial commit - Arrays as data structure

---
+++ src/com/smartdroidesign/arrays/Arrays.java
@@ -0,0 +1,70 @@
+package com.smartdroidesign.arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        // bit of a recap about arrays and how to access them
+
+
+        // This is a demonstration of the negative side of arrays
+        int[] intArray = new int[7];
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+//        int index = -1;
+//        for (int i =0; i <intArray.length; i++) {
+//            if (intArray[i] == 7) {
+//                index = i;
+//                break;
+//            }
+//        }
+//        System.out.println("index = " + index);
+//        int[] intArray = new int[5];
+//        int[] intArray2 = {1,2,3,4,5,6,7,8,9,10};
+//        int multiArray[][] = {{21,22,23,24,25}, {36,37,38,39,40}};
+//
+//
+//        int sum = 0;
+//        for (int x : intArray2) {
+//            sum = sum + x;
+//        }
+//        System.out.println(sum);
+//
+//        String shoppingList[] = {"Milk", "Bread", "Butter", "Fish", "Veggies"};
+//        for (String item : shoppingList) {
+//            System.out.println(item);
+//        }
+
+
+//        for (int row = 0; row <multiArray.length ; row++) {
+//            for (int column = 0; column <multiArray[row].length ; column++) {
+//                System.out.println(multiArray[row][column]);
+//            }
+//        }
+
+//        for (int[] f: multiArray) {
+//            for (int y: f) {
+//                System.out.println(y);
+//            }
+//        }
+
+//        for (int z: intArray2) {
+//            System.out.println(z);
+//        }
+
+//        intArray[0] = 51;
+//        intArray[1] = 52;
+//        intArray[2] = 53;
+//        intArray[3] = 54;
+//        intArray[4] = 55;
+
+//        for (int x: intArray) {
+//            System.out.println(x);
+//        }
+    }
+}
